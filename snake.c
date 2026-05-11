9fa4c89c18f87c97c993a8f8101ae703af7c13ac
Ashra1606 <your-email@example.com>
2026-05-11 14:37:13 +0600

    Fix draw_grid dimensions and implement full coordinate system

--- snake.c
+++ snake.c
@@ -3,6 +3,26 @@
 
 #define width 900
 #define height 600
+#define cell_size 15
+#define row height/cell_size
+#define col width/cell_size
+#define GRID_COLOR 0x444444FF
+#define line_width 2
+
+int draw_grid(SDL_Surface *surface)
+{
+    for(int i=0;i<row;i++)
+    {
+        SDL_Rect line_y={0,i*cell_size,width,line_width};
+        SDL_FillSurfaceRect(surface,&line_y,GRID_COLOR);
+    }
+     for(int j=0;j<col;j++)
+    {
+        SDL_Rect line_x={j*cell_size,0,line_width,height};
+        SDL_FillSurfaceRect(surface,&line_x,GRID_COLOR); 
+    }
+    
+}
 int main()
 {
     printf("Hello snake!\n");
@@ -21,6 +41,7 @@ int main()
     SDL_Surface* surface=SDL_GetWindowSurface(window);
     SDL_Rect rect={200,200,200,200};
     SDL_FillSurfaceRect(surface, &rect, 0xFFFFFFFF);
+    draw_grid(surface);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(5000);
     SDL_DestroyWindow(window);
